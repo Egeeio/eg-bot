0f6f21134f9871f70bf918d025f6288ae571e578
egee-irl <brian@egee.io>
2019-03-22 22:14:43 -0700

    linting

--- lib/listen.rb
+++ lib/listen.rb
@@ -1,30 +1,30 @@
 Thread.abort_on_exception = true
-require 'yaml'
-require 'timers'
-require 'docker'
-require 'discordrb'
-require './lib/listeners/generic'
-require './lib/handlers/discord'
+require "yaml"
+require "timers"
+require "docker"
+require "discordrb"
+require "./lib/listeners/generic"
+require "./lib/handlers/discord"
 
-# I'm listening
+# I"m listening
 module Listen
   def self.start
     @timers = Timers::Group.new
-    @bot = Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'], prefix: ENV['PREFIX'])
+    @bot = Discordrb::Commands::CommandBot.new(token: ENV["TOKEN"], prefix: ENV["PREFIX"])
     @bot.include! DiscordEvents
     @bot.ready do |event|
-      event.bot.game = YAML.load_file('./config.yml')['games'].sample
-      game_loop
+      event.bot.game = YAML.load_file("./config.yml")["games"].sample
+      game_loop()
     end
     @bot.run
   end
 
-  # def self.game_loop
-  #   puts 'and away we go'
-  #   @timers.now_and_every(30) do
-  #     GenericListener.listen(@bot, 'rust', %r{\/\w+(?=.joined)})
-  #     GenericListener.listen(@bot, 'minecraft', /(?<=\bUUID\sof\splayer\s)(\w+)/)
-  #   end
-  #   Thread.new { loop { @timers.wait } }
-  # end
+  def self.game_loop
+    puts "and away we go!!"
+    @timers.now_and_every(30) do
+      GenericListener.listen(@bot, "rust", %r{\/\w+(?=.joined)})
+      GenericListener.listen(@bot, "minecraft", /(?<=\bUUID\sof\splayer\s)(\w+)/)
+    end
+    Thread.new { loop { @timers.wait } }
+  end
 end
