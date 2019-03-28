a8ac19d25798c056e4a3228dbbc6d8ed402f12d9
egee-irl <brian@egee.io>
2019-03-28 15:50:25 -0700

    fixed bugs with parsing journal

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,21 +1,22 @@
 # Yes, linter
 class LogParser
+  attr_reader :players
   def initialize(games)
     @players = games
   end
 
   def parse
     @players.each do |key|
-      logs = get_logs(key)
+      logs = get_logs(key.first())
       logs.each_line do |log_line|
-        match = game_switch(game, log_line)
-        @players[key].push(match) if match.nil? == false
+        match = game_switch(key.first(), log_line)
+        @players[key.first()].push(match) if match.nil? == false && match.empty? == false
       end
     end
   end
 
   def get_logs(game)
-    `journalctl --since "15 seconds ago" --no-pager -u #{game}`
+    `journalctl --no-pager -u #{game}`
   end
 
   def game_switch(game, log_line)
@@ -30,11 +31,11 @@ class LogParser
   end
 
   def starbound(log_line)
-    log_line.match(/(['])(?:(?=(\\?))\2.)*?\1/).to_s() if log_line.include?(") connected")
+    log_line.match(/(['])(?:(?=(\\?))\2.)*?\1/).to_s().tr("'", "") if log_line.include?(") connected")
   end
 
   def rust(log_line)
-    log_line.match(%r{\/\w+(?=.joined)}).to_s([1..-1])
+    log_line.match(%r{\/\w+(?=.joined)}).to_s()[1..-1]
   end
 
   def minecraft(log_line)
