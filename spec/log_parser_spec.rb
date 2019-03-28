59cdd4f7aebb5dad98aecb0cedcdbe94344d2586
egee-irl <brian@egee.io>
2019-03-28 15:55:47 -0700

    simplified test a bit

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,12 +1,12 @@
 require "./lib/log_parser"
 
-RSpec.describe LogParser, "uhh" do
-  context "uhhhh" do
-    it "uhhh does something" do
-      games = { "starbound" => [], "rust" => [], "minecraft" => [] }
+RSpec.describe "Log Parser" do
+  context "When matches are found" do
+    it "Adds them to the array" do
+      games = { "starbound" => [], "rust" => [] }
       log_parser = LogParser.new(games)
       log_parser.parse()
-      puts log_parser.players
+      expect(log_parser.players.first).not_to be_empty
     end
   end
 end
