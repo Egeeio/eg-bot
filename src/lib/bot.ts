b2230abca3ff851b05407096ad5a2c6e980a8d9a
egee-irl <brian@egee.io>
2019-05-29 18:50:00 -0700

    refactored, added listen method

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,10 +1,20 @@
 import { Client} from "discord.js";
-import { GetChannel, GetGuild } from "./helpers";
+import { GetChannel, GetGuild, SendChannelMessage } from "./helpers";
 
-export default async function Listen() {
+export default async function Connect() {
   const client = new Client();
   await client.login(process.env.TOKEN);
-  const guild = await GetGuild(client, "561304574917607424");
-  const generalChannel = await GetChannel(guild, "general");
-  generalChannel.send("<3 bob");
+  Listen(client);
+}
+
+async function Listen(client: Client) {
+  const guild = await GetGuild(client, process.env.SERVER_ID!);
+  client.on("guildMemberAdd", async (member) => {
+    const general = await GetChannel(guild, "general");
+    SendChannelMessage(general, `ya boy ${member.displayName} has joined up`);
+  });
+  client.on("guildMemberRemove", async (member) => {
+    const debug = await GetChannel(guild, "he-man");
+    SendChannelMessage(debug, `**${member.displayName}** just bailed`);
+  });
 }
