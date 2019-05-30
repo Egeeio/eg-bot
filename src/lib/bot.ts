9b8fcd15a3301308c32f1b5fe5ed313061a1a6a2
egee-irl <brian@egee.io>
2019-05-29 18:08:03 -0700

    uses helpers

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,7 +1,10 @@
-import { Client } from "discord.js";
+import { Client} from "discord.js";
+import { GetChannel, GetGuild } from "./helpers";
 
 export default async function Listen() {
   const client = new Client();
   await client.login(process.env.TOKEN);
-  console.log("prepare to YEET");
+  const guild = await GetGuild(client, "561304574917607424");
+  const generalChannel = await GetChannel(guild, "general");
+  generalChannel.send("<3 bob");
 }
