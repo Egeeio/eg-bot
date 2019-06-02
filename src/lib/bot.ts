389f17f78261dabc4c742dd4f12e48e2fec4b0dc
egee-irl <brian@egee.io>
2019-06-02 02:11:06 -0700

    added envars

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -10,11 +10,11 @@ export default async function Connect() {
 async function Listen(client: Client) {
   const guild = await GetGuild(client, process.env.SERVER_ID!);
   client.on("guildMemberAdd", async (member) => {
-    const general = await GetChannel(guild, "general");
-    SendChannelMessage(general, `ya boy ${member.displayName} has joined up`);
+    const general = await GetChannel(guild, process.env.GENERAL_CHANNEL!);
+    SendChannelMessage(general, `ya boy ${member.displayName} has joined the server!`);
   });
   client.on("guildMemberRemove", async (member) => {
-    const debug = await GetChannel(guild, "he-man");
-    SendChannelMessage(debug, `**${member.displayName}** just bailed`);
+    const debug = await GetChannel(guild, process.env.DEBUG_CHANNEL!);
+    SendChannelMessage(debug, `**${member.displayName}** has left the server`);
   });
 }
